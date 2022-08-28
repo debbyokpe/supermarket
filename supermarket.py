4bc9d4c394e7501a217728ab84f70d34f6bca429
debbyokpe <okpedeborah7@gmail.com>
2022-08-28 21:49:31 +0100

    First Commit

---
+++ supermarket.py
@@ -0,0 +1,20 @@
+products = ["Milo" : 4,
+            "Cowbell": 5,
+            "Bournvita":5,
+            "Peak":5 ,
+            "Loya":6,
+            "Lux":7,
+            "Premier":6,
+            "CloseUp":2
+            "Oats":5
+            "Golden Morn":4
+            "Golden Spaghetti":3]
+
+customer = []
+
+numberOfItems = int(input("How many Items do you which to buy"))
+
+
+for x in range(6):
+    product = input("Which Product do you want to buy")
+    number = input("How many of the product do you want")
